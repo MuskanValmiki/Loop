b8df877d21d16cea0bf13cb5340b08ad9be0767d
MuskanValmiki <83203071+MuskanValmiki@users.noreply.github.com>
2021-06-08 12:21:34 +0530

    Add files via upload

---
+++ modulus_diff4.py
@@ -0,0 +1,14 @@
+num=int(input("enter a number"))
+i=1
+while i<num:
+    if i%4==1:
+        print(i)
+    i+=1
+
+i=1
+while i<6:
+    c=4*i-3
+    if i<18:
+        print(c)
+    i+=1
+# 1,5,9,13,17
