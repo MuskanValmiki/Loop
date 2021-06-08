3552bcf0708b4563a782b8eb769f334dd60c1d46
MuskanValmiki <83203071+MuskanValmiki@users.noreply.github.com>
2021-06-08 12:23:24 +0530

    Add files via upload

---
+++ Q1.py
@@ -0,0 +1,14 @@
+i=1
+while i<=100:
+    print(i)
+    i+=1
+
+# wethout multiply
+num1=int(input("enter any number="))
+num2=int(input("enter any number="))
+i=1
+sum=0
+while i<=num2:
+    sum+=num1
+    i+=1
+print(sum)
