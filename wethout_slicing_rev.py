b2bbff3fe13075cde4643837bb4fb1d6c07f6535
MuskanValmiki <83203071+MuskanValmiki@users.noreply.github.com>
2021-06-08 12:33:45 +0530

    Add files via upload

---
+++ wethout_slicing_rev.py
@@ -0,0 +1,19 @@
+name="my name is megha"
+i=-1
+e=[]
+a=name.split()
+while i>=(-len(a)):
+    e.append(a[i])
+    i-=1
+print(e)
+# ['megha', 'is', 'name', 'my']
+
+name="my name is muskan"
+i=-1
+e=[]
+a=name.split()
+while i>=(-len(a)):
+    e.append(a[i])
+    i-=1
+print(e)
+# ['muskan', 'is', 'name', 'my']
