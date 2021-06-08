98af1c4712167d3d3e19f21f369a092b3782a7d1
MuskanValmiki <83203071+MuskanValmiki@users.noreply.github.com>
2021-06-08 12:22:36 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,28 @@
+#input for one word
+name=input("enter any word which contain=")
+user=name.split(" ")
+i=0
+e=[]
+s=[]
+while i<len(user):
+    j=0
+    sum=0
+    while j <len(user[i]):
+        if user[i]>="a" and user[i]<="z":
+            sum=ord(user[i][j])-96
+        else:
+            sum=ord(user[i][j])-64
+        e.append(sum)
+        s.append(user[i][j])
+        j+=1   
+    k=0
+    while k<len(e):
+        if e[i]<e[k]:
+            tem=e[i]
+            e[i]=e[k]
+            e[k]=tem
+            s[i],s[k]=s[k],s[i]
+        k=k+1
+    i+=1
+print(e)
+print(s)
