f6da1067413650dbe9d9f57515f9b095a6ff306b
MuskanValmiki <83203071+MuskanValmiki@users.noreply.github.com>
2021-06-08 12:26:32 +0530

    Add files via upload

---
+++ Q7.py
@@ -0,0 +1,16 @@
+# wethout modulus
+i=0
+while i<=50:
+    if i!=0:
+        print(i)
+    i+=5
+
+i=5
+while i>=1:
+    j=1
+    while j<=5:
+        print(i,end="")
+        j+=1
+    print()
+    i-=1
+
