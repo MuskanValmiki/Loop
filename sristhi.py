eb5de9fe57516d86ca6b89372286e97f5ec22f09
MuskanValmiki <83203071+MuskanValmiki@users.noreply.github.com>
2021-06-08 12:29:55 +0530

    Add files via upload

---
+++ sristhi.py
@@ -0,0 +1,15 @@
+a=[2,4,3,1]
+i=0
+j=1
+user=int(input("enter a number"))
+l=len(a)-user
+while i<=l:
+    print(a[i],a[j])
+    j+=1
+    i+=1
+
+"""incelizezation
+while condition:
+    statement
+    increment
+this is the syntax of while loop"""
